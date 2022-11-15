62c7b320858ea014b3288d7394c5ced3540d6ba7
Santhosh Umapathi <Santhosh.Professional1@gmail.com>
2022-11-15 06:34:15 +0100

    FIX: Views updated

--- src/Presentation/screens/Home/ViewController.tsx
+++ src/Presentation/screens/Home/ViewController.tsx
@@ -1,6 +1,25 @@
+import {useEffect} from 'react';
+import TodoApiImpl from '../../../data/source/api/TodoApiImpl';
+import GetTodoUseCase from '../../../domain/usecase/Todo/getTodos';
 import TProps from './types';
 
 const useHomeViewController = (navigation: TProps['navigation']) => {
+  const UseCase = new GetTodoUseCase(new TodoApiImpl());
+
+  const getTodos = async () => {
+    try {
+      const results = await UseCase.getTodos();
+      console.log('🔥 --- getTodos --- results', results);
+    } catch (error) {
+      console.log('🔥 --- useHomeViewController --- error', error);
+    }
+  };
+
+  useEffect(() => {
+    getTodos();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
   const onUsersPress = () => {
     navigation.navigate('UsersStack');
   };
